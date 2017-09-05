3402e273da4e4e2514c80c0f98b07e272ff86c72
Ian Cobb <iancobb@Ians-MacBook-Pro.local>
2017-09-05 14:56:18 -0700

    Creating a gem

---
+++ lib/cobb_view_tool2.rb
@@ -0,0 +1,6 @@
+require "cobb_view_tool2/version"
+require "cobb_view_tool2/renderer"
+
+module CobbViewTool2
+  # Your code goes here...
+end
---
+++ lib/cobb_view_tool2/version.rb
@@ -0,0 +1,3 @@
+module CobbViewTool2
+  VERSION = "0.1.0"
+end
